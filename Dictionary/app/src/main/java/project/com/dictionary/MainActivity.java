fd84102756d5b01b09d5019d95e4e23b63420187
igrik <igrik.is.parich@gmail.com>
2017-02-22 15:08:45 +0300

    MainActivity

---
+++ Dictionary/app/src/main/java/project/com/dictionary/MainActivity.java
@@ -0,0 +1,17 @@
+package project.com.dictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Igor Ivanov
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
